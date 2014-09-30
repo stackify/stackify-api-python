9bc710cfc7f2594df47d89c5c3086116d011bd05
Matt Thompson <chameleonator@gmail.com>
2014-09-29 21:10:34 -0500

    Initial commit and project setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import re
+import ast
+
+version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('stackify/__init__.py') as f:
+    f = f.read()
+    version = ast.literal_eval(version_re.search(f).group(1))
+
+setup(
+    name='stackify',
+    version=version,
+    author='Matthew Thompson',
+    author_email='chameleonator@gmail.com',
+    packages=['stackify'],
+    url='https://github.com/stackify/stackify-python',
+    license=open('LICENSE.txt').readline(),
+    description='Stackify API for Python',
+    long_description=open('README.md').read(),
+    install_requires=[]
+)
+
