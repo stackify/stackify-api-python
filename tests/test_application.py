77847b29f059193fd1f47a8df7c1e7c76ad5246b
Matt Thompson <chameleonator@gmail.com>
2014-10-11 18:54:17 -0500

    More app test

--- tests/test_application.py
+++ tests/test_application.py
@@ -26,6 +26,32 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(config.api_key, 'test_apikey')
         self.assertEqual(config.api_url, 'test_apiurl')
 
+    def test_kwarg_mix(self):
+        env_map = {
+            'STACKIFY_APPLICATION': 'test2_appname',
+            'STACKIFY_ENVIRONMENT': 'test2_environment',
+        }
+
+        with patch.dict('os.environ', env_map):
+            config = get_configuration(api_key='test2_apikey', api_url='test2_apiurl')
+
+        self.assertEqual(config.application, 'test2_appname')
+        self.assertEqual(config.environment, 'test2_environment')
+        self.assertEqual(config.api_key, 'test2_apikey')
+        self.assertEqual(config.api_url, 'test2_apiurl')
+
+    def test_kwargs(self):
+        config = get_configuration(
+                    application = 'test3_appname',
+                    environment = 'test3_environment',
+                    api_key = 'test3_apikey',
+                    api_url = 'test3_apiurl')
+
+        self.assertEqual(config.application, 'test3_appname')
+        self.assertEqual(config.environment, 'test3_environment')
+        self.assertEqual(config.api_key, 'test3_apikey')
+        self.assertEqual(config.api_url, 'test3_apiurl')
+
 
 if __name__=='__main__':
     unittest.main()
