67319d133c23aee81b39f516174e0d6c2852a7fa
Matt Thompson <chameleonator@gmail.com>
2014-10-11 23:16:20 -0500

    Fix patch and add more testing

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -46,12 +46,31 @@ class TestListener(unittest.TestCase):
     @patch('stackify.handler.LogMsg')
     @patch.object(StackifyListener, 'send_group')
     @patch('stackify.http.HTTPClient.identify_application')
-    def test_not_identified(self, logmsg, send_group, ident):
+    def test_not_identified(self, ident, send_group, logmsg):
         '''The HTTPClient identifies automatically if needed'''
-        q = queue.Queue(1)
-        listener = StackifyListener(queue_=q, config=self.config)
+        listener = StackifyListener(queue_=Mock(), config=self.config)
         listener.handle(Mock())
         self.assertTrue(ident.called)
+
+    @patch('stackify.handler.LogMsg')
+    @patch('stackify.handler.LogMsgGroup')
+    @patch('stackify.handler.HTTPClient.POST')
+    def test_send_group_if_needed(self, post, logmsggroup, logmsg):
+        '''The listener sends groups of messages'''
+        listener = StackifyListener(queue_=Mock(), max_batch=3, config=self.config)
+        listener.http.identified = True
+
+        listener.handle(1)
+        self.assertFalse(post.called)
+        listener.handle(2)
+        self.assertFalse(post.called)
+        self.assertEqual(len(listener.messages), 2)
+        listener.handle(3)
+        self.assertTrue(post.called)
+        self.assertEqual(len(listener.messages), 0)
+        listener.handle(4)
+        self.assertEqual(post.call_count, 1)
+        self.assertEqual(len(listener.messages), 1)
 
 
 if __name__=='__main__':
