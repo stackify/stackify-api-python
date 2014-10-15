97ccaf58fb3f028a5de0f252af611ebb5cf2cf84
Matt Thompson <chameleonator@gmail.com>
2014-10-15 14:06:13 -0500

    Fixing setup for pypi submission

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup
 import re
 import ast
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print('warning: pypandoc module not found, could not convert Markdown to RST')
+    read_md = lambda f: open(f).read()
+
 version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('stackify/__init__.py') as f:
@@ -15,10 +22,13 @@ setup(
     author = 'Matthew Thompson',
     author_email = 'chameleonator@gmail.com',
     packages = ['stackify'],
-    url = 'https://github.com/stackify/stackify-python',
+    url = 'https://github.com/stackify/stackify-api-python',
     license = open('LICENSE.txt').readline(),
     description = 'Stackify API for Python',
-    long_description = open('README.md').read(),
+    long_description = read_md('README.md'),
+    download_url = 'https://github.com/stackify/stackify-api-python/tarball/0.0.1',
+    keywords = ['logging', 'stackify', 'exception'],
+    classifiers=["Programming Language :: Python"],
     install_requires = [
         'retrying>=1.2.3',
         'requests>=2.4.1'
