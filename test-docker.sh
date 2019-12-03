af162e73df85b749d02dc9c39f8cd228fb2e9dcc
jaygel <adoptante.elpedio@gmail.com>
2019-12-03 23:40:50 +0800

    Python 3.8 support (#9)
    Changes:
    - update proper versions for pythons
    - add support for python 3.8
    - add docket tests for different versions

---
+++ test-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+VERSIONS=('2.7' '3.4' '3.5' '3.6' '3.7' '3.8')
+# VERSIONS=('2.7')
+
+for i in "${VERSIONS[@]}"
+do
+
+    if [[ "$(docker images -q stackify-python-api-test-${i}:latest 2> /dev/null)" != "" ]]; then
+        echo "Delete stackify-python-api-test-${i}..."
+        docker rm stackify-python-api-test-${i} &>/dev/null
+        docker rmi stackify-python-api-test-${i}:latest &>/dev/null
+    fi
+
+    echo "Building stackify-python-api-test-${i}..."
+    docker build --no-cache --build-arg from_version=${i} --build-arg version=${i} --file docker/stackify-python-api-test . -t stackify-python-api-test-${i}:latest
+
+    echo "Running stackify-python-api-test-${i}..."
+    docker run --network="host" --name "stackify-python-api-test-${i}"  stackify-python-api-test-${i}:latest
+
+    echo "Delete stackify-python-api-test-${i}..."
+    docker rm stackify-python-api-test-${i} &>/dev/null
+    docker rmi stackify-python-api-test-${i}:latest &>/dev/null
+
+done
+
+echo "Done"
